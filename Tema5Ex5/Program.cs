feaf5d5489fd3e634f3ba2cb15d329dcaa95f09a
Gergoa123 <gergo3124@gmail.com>
2022-06-09 16:43:47 +0300

    Add project files.

---
+++ Tema5Ex5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tema5Ex5
+{
+    //Ex 5
+    //Un program care va normaliza un sir de caractere citit de la tastatura astfel incat fiecare cuvant din
+    //sir sa inceapa cu MAJUSCULA, restul literelor fiind transformate in litere mici
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Introduceti string-ul!");
+            string sir = Console.ReadLine();
+            string lowercase = sir.ToLower();
+
+            string[] split = lowercase.Split(' ',  StringSplitOptions.RemoveEmptyEntries);
+
+            //foreach (string fragment in split)
+            //{
+            //    fragment = (Char.ToUpper(fragment[0]) + fragment.Substring(1));
+            //}
+            //
+
+            for (int i = 0; i < split.Length; ++i)
+            {
+                split[i] = (Char.ToUpper(split[i][0]) + split[i].Substring(1));
+            }
+
+
+            foreach (string fragment in split)
+            {
+                Console.Write(fragment + " ");
+            }
+        }
+    }
+}
